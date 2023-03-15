ffaebf505a9123c4fcc39cdd7e7b55d3fc91f5a6
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-15 21:28:24 +0700

    update nested loop

--- ex2.js
+++ ex2.js
@@ -10,6 +10,23 @@ Write a JavaScript program to construct the following pattern, using a nested fo
 
 */
 
+for (let i=0; i<=4 ; i++){
+  let temp = ""
+  for(let j=0 ;j <= i ; j++){
+    temp += "*"
+  }
+console.log(temp)
+}
+
+//'*'
+//'**'
+//'***'
+//'****'
+//'*****'
+
+
+
+/* น่าจะผิด ไม่ใช่ nested loop
 let add = (pt) => {
   let dokjan = "";
   for(let i = 0 ; i < pt ; i++){
@@ -22,3 +39,4 @@ add(5);
 //'***'
 //'****'
 //'*****'
+*/
