5ad906d22ccc42b39c3184a8ec0cb3ff38007a72
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-15 18:33:34 +0700

    done ex.1

---
+++ ex1.js
@@ -0,0 +1,22 @@
+/*
+Exercise #1
+Part 1
+There are three people waiting for the bank. Their names, in order, are Sofia, David, and Juan. Create an array that has these three names in order.
+
+Part 2
+Two more people get added to the back of the line - Sara and Augustin. The first person in line is called to the teller. What does the queue look like?
+
+Part 3
+It turns out David was saving a spot for his friend Renata. She shows up and goes behind him in the line. One more person (Elena) shows up and goes to the end of the line. What does the queue look like?
+*/
+//part1
+let waitbank = ['Sofia', 'David', 'Juan']
+//part2
+waitbank.push('Sara','Augustin')
+console.log(waitbank) //[ 'Sofia', 'David', 'Juan', 'Sara', 'Augustin' ]
+
+//part3
+waitbank.splice(2,0,'Renata')
+waitbank.push('Elena')
+console.log(waitbank)
+//[ 'Sofia', 'David', 'Renata', 'Juan', 'Sara', 'Augustin', 'Elena' ]
